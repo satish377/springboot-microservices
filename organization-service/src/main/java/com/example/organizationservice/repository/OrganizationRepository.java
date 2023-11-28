f9c27b45a14d65f0a718ff7ee808a6f0dba9bb6b
Venkata Satish Peruri <103985384+satish377@users.noreply.github.com>
2023-11-28 23:31:52 +0530

    Delete organization-service directory

--- organization-service/src/main/java/com/example/organizationservice/repository/OrganizationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.organizationservice.repository;
-
-import com.example.organizationservice.entity.Organization;
-import org.hibernate.query.criteria.JpaDerivedRoot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization,Long> {
-}
