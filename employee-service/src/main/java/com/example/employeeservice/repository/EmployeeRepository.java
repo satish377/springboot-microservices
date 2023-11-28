6b913658171ac9d7a75d5d3819d2715a449517bf
Venkata Satish Peruri <103985384+satish377@users.noreply.github.com>
2023-11-28 23:33:16 +0530

    Delete employee-service directory

--- employee-service/src/main/java/com/example/employeeservice/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeeservice.repository;
-
-import com.example.employeeservice.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
