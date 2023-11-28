3fc3fe23e63024f3025d0c5150025406698f36aa
Venkata Satish Peruri <103985384+satish377@users.noreply.github.com>
2023-11-28 23:42:13 +0530

    Add files via upload

---
+++ springboot-kafka-tutorial/springboot-kafka-tutorial/src/main/java/com/example/springbootkafkatutorial/controller/JsonMessageController.java
@@ -0,0 +1,27 @@
+package com.example.springbootkafkatutorial.controller;
+
+import com.example.springbootkafkatutorial.Kafka.JsonKafkaProducer;
+import com.example.springbootkafkatutorial.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/kafka")
+public class JsonMessageController {
+
+    private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json Message sent to kafka topic");
+    }
+
+}
---
+++ springboot-kafka-tutorial/springboot-kafka-tutorial/src/main/java/com/example/springbootkafkatutorial/payload/User.java
@@ -0,0 +1,40 @@
+package com.example.springbootkafkatutorial.payload;
+
+public class User {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private int id;
+    private String firstName;
+    private  String lastName;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
